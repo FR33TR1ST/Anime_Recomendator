a1f064902b780f4b0b5f62f5514565da4610d0e2
jeremiasAgueroDiaz <jaguerodiaz@escuelasproa.edu.ar>
2022-12-03 03:00:45 -0300

    comit inicial sin conexion a base de datos

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-//creation of the server
-const express = require('express')
-const app = express()
-const port = 3000
+const mongoose = require("mongoose");
+const app = require("./app");
+const dotenv = require("dotenv");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//Carga de variables de entorno
+dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
 
+//Conexión al cloud de Mongodb Atlas ...
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+    })
+    .then((con) => {
+        //console.log(con.connections);
+        console.log("Connected to database");
+    });
+
+const port = 3000;
+//Corremos el servidor en el puerto seleccionado
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Servidor corriendo en el puerto ${port} correctamente`);
+});
